8fb87725751541371eca919c7f81c6ab6b9c2762
Giuliana Saborío <giulianasaborio@gmail.com>
2023-05-30 22:15:23 -0600

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# cineapp
+
+1. Copiar el .env.template y renovarlo a .env
+2. Cambiar las variables de entorno (The MovieDB)
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:cineapp/config/router/app_router.dart';
+import 'package:cineapp/config/theme/app_theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+    );
+  }
+}
